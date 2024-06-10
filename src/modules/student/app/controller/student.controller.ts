bce5a414887fc3ae32c9f306b2e83d3bd14b85ca
lucas cortez <lucasc.sanches65@gmail.com>
2024-06-10 15:01:05 -0300

    chore: student controller

--- src/modules/student/app/controller/student.controller.ts
+++ src/modules/student/app/controller/student.controller.ts
@@ -1,10 +1,34 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { IController } from "src/core/abstracts/controller";
 import { injectable } from "tsyringe";
+import { GetStudentByTeacherUseCase } from "../use-cases/get-student-by-teacher";
+import { GetStudentsByTeacherUseCase } from "../use-cases/get-students-by-teacher";
+import { InviteStudentUseCase } from "../use-cases/invite-student";
 
 @injectable()
 export class StudentController implements IController {
+  constructor(
+    private readonly getStudentByTeacherUseCase: GetStudentByTeacherUseCase,
+    private readonly getStudentsByTeacherUseCase: GetStudentsByTeacherUseCase,
+    private readonly inviteStudentUseCase: InviteStudentUseCase,
+
+  ) {}
+
   async register(registerInstance: FastifyInstance) {
     registerInstance.register(async (instance) => {}, { prefix: "/student" });
   }
+
+  async getStudentByTeacher(request: FastifyRequest, reply: FastifyReply) {
+    const dto = GetStudentByTeacherDto.validate(request.body);
+
+    const data = await this.getStudentByTeacherUseCase.execute({
+      studentId:
+    })
+
+    return data
+  }
+
+  async getStudentsByTeacher(request: FastifyRequest, reply: FastifyReply) {}
+
+  async inviteStudent(request: FastifyRequest, reply: FastifyReply) {}
 }
